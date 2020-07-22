117f3e68b7347379411d62d1b672e6cb5f242690
benoitdemaegdt <benoit.demaegdt@gmail.com>
2020-07-22 08:08:17 +0200

    bump sncf api from 740 to 750

--- _utils/sncf.js
+++ _utils/sncf.js
@@ -21,7 +21,7 @@ async function getTgvmaxHours(origin, destination, fromTime, toTime, tgvmaxNumbe
   try {
     while (keepSearching) {
       const config = {
-        url: 'https://wshoraires.oui.sncf/m740/vmd/maq/v3/proposals/train',
+        url: 'https://wshoraires.oui.sncf/m750/vmd/maq/v3/proposals/train',
         method: 'POST',
         headers: {
           Accept: 'application/json',
